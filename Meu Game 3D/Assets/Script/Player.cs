b608823106dc5582248299dea3845efc75b1dbd9
Alisson Breno <breno.alisson@escolar.ifrn.edu.br>
2024-08-26 16:02:42 -0300

    Primeira Fase(1)
    Jogo parkour bolinha

--- Meu Game 3D/Assets/Script/Player.cs
+++ Meu Game 3D/Assets/Script/Player.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Net.Mail;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
-using UnityEngine.SceneManager
+using UnityEngine.SceneManagement;
+
 public class Player : MonoBehaviour
 {
-    public int velocidade = 10; 
+    public int velocidade = 10;
+    public int forcaPulo = 7;
     Rigidbody rb;
    
     void Start()
@@ -16,12 +18,19 @@ public class Player : MonoBehaviour
 
    
     void Update()
-    {   Debug.Log(message"update");
+    {   
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         Vector3 direcao = new Vector3(h, 0, v);
         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+        }
+        
+        
+        
         if(transform.position.y <= -10)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);

b75682f2ea49a2f02f02ea3a1a44868968a9268c
Alisson Breno <breno.alisson@escolar.ifrn.edu.br>
2024-08-26 15:26:17 -0300

    ;

--- Meu Game 3D/Assets/Script/Player.cs
+++ Meu Game 3D/Assets/Script/Player.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using System.Net.Mail;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
-
+using UnityEngine.SceneManager
 public class Player : MonoBehaviour
 {
-    public int velocidade = 10;
+    public int velocidade = 10; 
     Rigidbody rb;
-    // Start is called before the first frame update
+   
     void Start()
     {
         TryGetComponent(out rb);
     }
 
-    // Update is called once per frame
+   
     void Update()
-    {
+    {   Debug.Log(message"update");
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         Vector3 direcao = new Vector3(h, 0, v);
         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+
+        if(transform.position.y <= -10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
